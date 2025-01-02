016acc71a1b586081902b523a770bedf34a0176a
Adhim-IT <xiaochilde05@gmail.com>
2025-01-03 06:51:10 +0700

    adhim-done

--- assets/script.js
+++ assets/script.js
@@ -27,3 +27,12 @@ else{
 }
 
 window.onload=animateText;
+
+document.addEventListener("scroll", function () {
+  const navbar = document.querySelector(".navbar");
+  if (window.scrollY > 50) {
+    navbar.classList.add("scrolled");
+  } else {
+    navbar.classList.remove("scrolled");
+  }
+});
